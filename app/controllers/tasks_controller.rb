c965aa8b4ff96cb11c597bd93eb20dc66d3f0ad9
Michael Grossman & Corey Norris <student@epicodus.com>
2014-03-31 11:43:11 -0700

    add ability to create tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,14 @@ class TasksController < ApplicationController
     @tasks = Task.all
     render('tasks/index.html.erb')
   end
+
+  def new
+    @task = Task.new
+    render('tasks/new.html.erb')
+  end
+
+  def create
+      @task = Task.create(:description => params[:description])
+      render('tasks/success.html.erb')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 ToDo::Application.routes.draw do
   match('tasks', {:via => :get, :to => 'tasks#index'})
+  match('tasks/new', {:via => :get, :to => 'tasks#new'})
+  match('tasks', {:via => :post, :to => 'tasks#create'})
 end
